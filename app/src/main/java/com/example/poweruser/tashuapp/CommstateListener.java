ec708b95daa92cd1c37f0aed4d98c660e65938f9
sosobal <sosobal1024@naver.com>
2018-12-17 03:17:54 +0900

    first commit

---
+++ app/src/main/java/com/example/poweruser/tashuapp/CommstateListener.java
@@ -0,0 +1,45 @@
+package com.example.poweruser.tashuapp;
+
+import android.content.Context;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class CommstateListener extends PhoneStateListener {
+    protected TelephonyManager telephonyManager;
+    protected Context context;
+    public int nRssi;
+
+    public CommstateListener(TelephonyManager telephonyManager, Context context) {
+        this.telephonyManager = telephonyManager;
+        this.context = context;
+    }
+
+    @Override
+    public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+        super.onSignalStrengthsChanged(signalStrength);
+        int nPhoneType = telephonyManager.getPhoneType();
+        if (nPhoneType == TelephonyManager.PHONE_TYPE_GSM)
+            nRssi = signalStrength.getGsmSignalStrength();
+        else if (nPhoneType == TelephonyManager.PHONE_TYPE_CDMA)
+            nRssi = signalStrength.getCdmaDbm();
+        //Toast.makeText(context, "RSSI = " + nRssi, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCallStateChanged(int state, String incomingNumber) {
+        super.onCallStateChanged(state, incomingNumber);
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                Toast.makeText(context, "Ringing: " + incomingNumber, Toast.LENGTH_SHORT).show();
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                Toast.makeText(context, "Offhook: " + incomingNumber, Toast.LENGTH_SHORT).show();
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                Toast.makeText(context, "Idle", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/poweruser/tashuapp/PhoneCallService.java
@@ -0,0 +1,51 @@
+package com.example.poweruser.tashuapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class PhoneCallService extends Service {
+    protected PhoneCallReceiver phoneCallReceiver;  // Outgoing
+    protected CommstateListener commStateListener;  // Receiving
+    protected TelephonyManager telephonyManager;
+
+    public PhoneCallService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        phoneCallReceiver = new PhoneCallReceiver();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
+        registerReceiver(phoneCallReceiver, intentFilter);
+        intentFilter = new IntentFilter(Intent.ACTION_CALL_BUTTON);
+        registerReceiver(phoneCallReceiver, intentFilter);
+        telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        commStateListener = new CommstateListener(telephonyManager, this);
+        telephonyManager.listen(commStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service started.", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service stopped.", Toast.LENGTH_SHORT).show();
+        unregisterReceiver(phoneCallReceiver);
+        telephonyManager.listen(commStateListener, PhoneStateListener.LISTEN_NONE);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
